5ce536ff37670187015e95117acb314886abeb44
aly <alyrodob@gmail.com>
2023-05-21 23:04:39 -0300

    :poop: The end...
    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
 
-    <main className="flex h-full w-full flex-col items-center justify-center p-4 lg:h-screen bg-black">
+    <main className="flex h-screen w-screen flex-col items-center justify-center p-4 lg:h-screen bg-black">
       <h1 className="text-8xl">💔</h1>
     </main>
   )
